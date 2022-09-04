075d877886d6c6907b004d06d484debcf833d19d
BRCZ1N <brunocamposo.r@gmail.com>
2022-09-04 20:59:34 -0300

    Alterações

--- AnalisadorLexico/src/com/analisadorlexico/LexicalAnaliser.java
+++ AnalisadorLexico/src/com/analisadorlexico/LexicalAnaliser.java
@@ -33,192 +33,394 @@ public class LexicalAnaliser {
 		int state = 0;
 		String lexeme = null;
 		char currentChar = file.get(pos);
-	
+
 		while (!isEOF(pos)) {
-			
+
 			if (state == 0) {
 
 				lexeme = "";
 
+				// Identificador
 				if (isLetter(currentChar)) {
 
 					lexeme += currentChar;
 					state = 1;
 					pos++;
 
-				}else if(currentChar == '-') {
+				} else if (currentChar == '-') {
 
 					lexeme += currentChar;
 					state = 2;
 					pos++;
-		
-					
-				}else if(currentChar == '&') {
-					
+
+				} else if (currentChar == '&') {
+
 					lexeme += currentChar;
 					state = 18;
 					pos++;
-		
-				}else if(currentChar == '!') {
-					if (nextChar() == '=') {
-						lexeme += currentChar;
-						state = 20;
-						pos++;
-					}else{
-						
-						// retornar o operador de negação
-					}
-					
-					
-				}else if(currentChar == '|') {
-				
+
+				} else if (currentChar == '!') {
+
+					lexeme += currentChar;
+					state = 15;
+					pos++;
+
+				} else if (currentChar == '|') {
+
 					lexeme += currentChar;
 					state = 16;
 					pos++;
-					
-				}else if(currentChar == '=') {
-					if(nextChar() == '=') {
-						lexeme += currentChar;
-						state = 25;
-						pos++;
-						
-					}else {
-						// retornar o operador relacional = 
-					}
-					
-				}else if(currentChar == '<') {
-					if(nextChar() == '=') {
-						lexeme += currentChar;
-						state = 23;
-						pos++;
-					}else {  	
-						// retornar o operador <
-					}
-					
-					
-				}else if(currentChar == '>') {
-					if(nextChar() == '=') {
-						lexeme += currentChar;
-						state = 21;
-						pos++;
-					}else {
-						// return o operador >
-					}
-					
-					
-				}else if(currentChar == '+') {
-					
-					
+
+				} else if (currentChar == '=') {
+
+					lexeme += currentChar;
+					state = 25;
+					pos++;
+
+				} else if (currentChar == '<') {
+
+					lexeme += currentChar;
+					state = 23;
+					pos++;
+
+				} else if (currentChar == '>') {
+
+					lexeme += currentChar;
+					state = 21;
+					pos++;
+				} else if (currentChar == '+') {
+
 					lexeme += currentChar;
 					state = 7;
 					pos++;
-					
-				}else if(currentChar == '*') {
-					
+
+				} else if (currentChar == '*') {
+
 					lexeme += currentChar;
 					state = 9;
 					pos++;
 
-				}else if(currentChar == '/') {
-					if(nextChar() == '/') {
-						lexeme += currentChar;
-						state = 11;
-						pos++;
-					}else if (nextChar()=='*') {
-						lexeme += currentChar;
-						state = 12;
-					}else {
-						// return operador /
-					}
-					
-					
-				}else if(isDigit(currentChar)) {
-					
+				} else if (currentChar == '/') {
+
+					lexeme += currentChar;
+					state = 10;
+					pos++;
+
+				} else if (isDigit(currentChar)) {
+
 					lexeme += currentChar;
 					state = 3;
 					pos++;
-					
-				}else if(currentChar == '"') {
-					
+
+				} else if (currentChar == '"') {
+
 					lexeme += currentChar;
 					state = 28;
 					pos++;
-					
-				}else {
-					
-				//NÃO SEI O QUE É AQ	
-					
-				}
-				
-			}else if (state == 18) {
-				if(currentChar == '&') {
-					lexeme +=  currentChar;
-					state = 0;
-					pos++;
-					// return aq
-				}else {
-					// erro lexico
+
+				} else {
+
+					// NÃO SEI O QUE É AQ
+
 				}
 
-			}else if (state == 16) {
-				if(currentChar == '|') {
+			} else if (state == 1) {
+
+				if (isLetter(currentChar) || isDigit(currentChar) || currentChar == '_') {
+
+					lexeme += currentChar;
+					state = 1;
+					pos++;
+
+				} else if (currentChar == '\t') {
+
 					lexeme += currentChar;
 					state = 0;
 					pos++;
-					// return
-				}else {
-					//erro
+
+				} else {
+
+					// PENSANDO NOS ERROS
+
 				}
-			
-			}else if(state == 9) {
-				//return operador *
-			}else if(state == 20) {
-				if(currentChar == '=') {
+
+			} else if (state == 2) {
+
+				if (currentChar == '-') {
+
+					lexeme += currentChar;
+					state = 6;
+					pos++;
+
+				} else if (isDigit(currentChar)) {
+
+					lexeme += currentChar;
+					state = 3;
+					pos++;
+
+				} else {
+
+					// ERRO LEXICO
+
+				}
+
+			}
+
+			else if (state == 3) {
+
+				if (isDigit(currentChar)) {
+
+					lexeme += currentChar;
+					state = 3;
+					pos++;
+
+				} else if (currentChar == '.') {
+
+					lexeme += currentChar;
+					state = 4;
+					pos++;
+
+				} else {
+
+					// ERRO LEXICO
+
+				}
+
+			} else if (state == 4) {
+
+				if (isDigit(currentChar)) {
+
+					lexeme += currentChar;
+					state = 5;
+					pos++;
+
+				} else {
+
+					// ERRO LEXICO
+
+				}
+
+			} else if (state == 5) {
+
+				if (isDigit(currentChar)) {
+
+					lexeme += currentChar;
+					state = 5;
+					pos++;
+
+				} else {
+
 					lexeme += currentChar;
 					state = 0;
 					pos++;
-				}else {
-					//erro
 				}
-			}else if(state == 25) {
-				if(currentChar == '=') {
+
+			} else if (state == 6) {
+
+				// RETURN ALGO AQ
+
+			} else if (state == 7) {
+
+				// FALTA FAZER
+
+			} else if (state == 8) {
+
+				// FALTA FAZER
+
+			} else if (state == 9) {
+
+				// RETURN ALGO AQ
+
+			} else if (state == 10) {
+
+				if (currentChar == '/') {
+
+					lexeme += currentChar;
+					state = 11;
+					// RETURN OPERADOR DE DIVISÃO
+					pos++;
+
+				} else if (currentChar == '*') {
+
+					lexeme += currentChar;
+					state = 12;
+					pos++;
+
+				}
+
+			} else if (state == 11) {
+
+				if (currentChar == '/') {
+
 					lexeme += currentChar;
 					state = 0;
 					pos++;
-					
-				}else {
-					//erro
+
 				}
-			}else if(state == 23) {
-				if(currentChar == '=') {
+
+			} else if (state == 12) {
+
+				if (currentChar == '*') {
+
 					lexeme += currentChar;
-					state = 0;
+					state = 13;
 					pos++;
-				}else {
-					//erro
+
+				} else {
+
+					lexeme += currentChar;
+					state = 12;
+					pos++;
 				}
-			}else if(state == 21) {
+			} else if (state == 13) {
+
+				if (currentChar == '/') {
+
+					lexeme += currentChar;
+					state = 14;
+					pos++;
+
+				} else {
+
+					lexeme += currentChar;
+					state = 12;
+					pos++;
+
+				}
+
+			} else if (state == 14) {
+
+				// RETURN ALGO AQ
+
+			} else if (state == 15) {
+
 				if (currentChar == '=') {
 					lexeme += currentChar;
-					state = 0;
+					state = 20;
 					pos++;
-				}else {
-					//erro
+
+				} else {
+
+					// erro lexico
+
 				}
-			}else if(state == 11) {
-				if (currentChar == '/') {
+
+			} else if (state == 16) {
+				if (currentChar == '|') {
 					lexeme += currentChar;
-					state = 0;
+					state = 17;
 					pos++;
+					// return
+				} else {
+					// erro
 				}
-			}else if(state == 12) {
-				if (currentChar == '*') {
+
+			} else if (state == 17) {
+
+				// RETURN
+
+			} else if (state == 18) {
+
+				if (currentChar == '&') {
 					lexeme += currentChar;
+					state = 19;
+					pos++;
+
+				} else {
+
+					// erro lexico
+
+				}
+
+			} else if (state == 19) {
+
+				// RETURN ALGO AQ
+
+			} else if (state == 20) {
+
+				// RETURN ALGO
+
+			} else if (state == 21) {
+
+				if (currentChar == '=') {
+
+					lexeme += currentChar;
+					state = 22;
+					pos++;
+
+				} else {
+
+					// ERRO LEXICO
+
+				}
+
+			} else if (state == 22) {
+
+				// RETURN ALGO AQ
+
+			} else if (state == 23) {
+
+				if (currentChar == '=') {
+
+					lexeme += currentChar;
+					state = 24;
+					pos++;
+
+				} else {
+
+					// erro
+
+				}
+
+			} else if (state == 24) {
+
+				// RETURN ALGO AQ
+
+			} else if (state == 25) {
+
+				if (currentChar == '=') {
+					lexeme += currentChar;
+					state = 26;
+					pos++;
+					// return
+				} else {
+					// erro
+				}
+
+			} else if (state == 26) {
+
+				// RETURN ALGO AQ
+
+			} else if (state == 27) {
+
+				// RETURN ALGO AQ
+
+			} else if (state == 28) {
+
+				if (isLetter(currentChar) || isDigit(currentChar)
+						|| (currentChar >= 32 && currentChar <= 126 && currentChar != 34)) {
+
+					lexeme += currentChar;
+					state = 28;
+					pos++;
+
+				} else if (currentChar == '"') {
+
+					lexeme += currentChar;
+					state = 29;
+					pos++;
+
+				} else {
+
+					// ERRO LEXICO
 				}
 			}
-	}
+
+		}
+
 		return null;
-}
+
+	}
 
 	private boolean isDigit(char c) {
 
