9899e54e25956b0f13d5dab9560f22b46de50405
BRCZ1N <brunocamposo.r@gmail.com>
2022-09-04 17:38:17 -0300

    Alterações

--- AnalisadorLexico/src/com/analisadorlexico/LexicalAnaliser.java
+++ AnalisadorLexico/src/com/analisadorlexico/LexicalAnaliser.java
@@ -11,16 +11,6 @@ public class LexicalAnaliser {
 	private LinkedList<Character> file;
 	private int pos = 0;
 
-	// public static int TokenReservedWord = 3;
-	/*
-	 * public static int TokenNumber = 1; public static int TokenAritmeticOperator =
-	 * 2; public static int TokenIdentifier = 4; public static int TokenDigit = 5;
-	 * public static int TokenLetter = 6; public static int TokenRelationalOperator
-	 * = 7; public static int TokenLogicOperator = 8; public static int
-	 * TokenDelimiter = 9; public static int TokenString = 10; public static int
-	 * TokenSymbol = 11; public static int TokenEspace = 12;/ public boolean isDigit
-	 */
-
 	public LexicalAnaliser(String fileName) {
 
 		String contentFile;
@@ -41,60 +31,110 @@ public class LexicalAnaliser {
 	public Token scanFile() {
 
 		int state = 0;
-		String lexeme = "";
-
+		String lexeme = null;
+		char currentChar = file.get(pos);
+	
 		while (!isEOF(pos)) {
-
+			
 			if (state == 0) {
 
 				lexeme = "";
-				
-				if (isLetter(file.get(pos))) {
 
-					lexeme += file.get(pos);
+				if (isLetter(currentChar)) {
+
+					lexeme += currentChar;
 					state = 1;
 					pos++;
 
-				}if(isLogicalOperator(file.get(pos))) {
-					
-					lexeme += file.get(pos);
-					
-					if((nextChar() == '&' || nextChar() == '|') && nextChar() != '!') {
-						
-						lexeme += nextChar();
-						
-					}
-					
-					state = 0;
-					return new Token(Token.SiglaToken.TK_OPERADOR_LOGICO.name(),lexeme);
+				}else if(currentChar == '-') {
+
+					lexeme += currentChar;
+					state = 2;
+					pos++;
 		
-				}
-
-			} else if (state == 1) {
-
-				if (isLetter(file.get(pos)) || isDigit(file.get(pos)) || file.get(pos) == '_') {
-
-					lexeme += file.get(pos);
-					state = 1;
+					
+				}else if(currentChar == '&') {
+					
+					lexeme += currentChar;
+					state = 18;
+					pos++;
+		
+				}else if(currentChar == '!') {
+					
+					lexeme += currentChar;
+					state = 19;
+					pos++;
+					
+				}else if(currentChar == '|')
+				
+					lexeme += currentChar;
+					state = 16;
+					pos++;
+					
+				}else if(currentChar == '=') {
+					
+					lexeme += currentChar;
+					state = 25;
+					pos++;
+					
+				}else if(currentChar == '<') {
+					
+					lexeme += currentChar;
+					state = 23;
+					pos++;
+					
+					
+				}else if(currentChar == '>') {
+					
+					lexeme += currentChar;
+					state = 21;
+					pos++;
+					
+				}else if(currentChar == '+') {
+					
+					lexeme += currentChar;
+					state = 7;
+					pos++;
+					
+				}else if(currentChar == '*') {
+					
+					lexeme += currentChar;
+					state = 9;
 					pos++;
 
-				} else {
-
-					state = 0;
+				}else if(currentChar == '/') {
+					
+					lexeme += currentChar;
+					state = 10;
 					pos++;
-					return new Token(Token.SiglaToken.TK_IDENTIFICADOR.name(), lexeme);
-
+					
+				}else if(isDigit(currentChar)) {
+					
+					lexeme += currentChar;
+					state = 3;
+					pos++;
+					
+				}else if(currentChar == '"') {
+					
+					lexeme += currentChar;
+					state = 28;
+					pos++;
+					
+				}else {
+					
+				//NÃO SEI O QUE É AQ	
+					
 				}
-
+				
 			}
 
-		}
+		
 
-		return null;
+	return null;
 
 	}
 
-	public boolean isDigit(char c) {
+	private boolean isDigit(char c) {
 
 		if (c >= '0' || c <= '9') {
 
@@ -106,9 +146,9 @@ public class LexicalAnaliser {
 
 	}
 
-	public boolean isLetter(char c) {
+	private boolean isLetter(char c) {
 
-		if (c >= 'a' || c <= 'z' || c >= 'A' || c <= 'Z') {
+		if ((c >= 'a' || c <= 'z') || (c >= 'A' || c <= 'Z')) {
 
 			return true;
 
@@ -179,6 +219,18 @@ public class LexicalAnaliser {
 	public char nextChar() {
 
 		return file.get(pos++);
+
+	}
+
+	public char previousChar() {
+
+		return file.get(pos--);
+
+	}
+
+	public char doubleNextChar() {
+
+		return file.get(pos + 2);
 
 	}
 
